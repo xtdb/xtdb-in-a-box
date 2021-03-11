4998db33d866a695c7d10049eb28fdada88a46fd
Steven Deobald <steven@deobald.ca>
2021-03-10 20:15:53 -0600

    Marking logging requirements with comments

--- clj/project.clj
+++ clj/project.clj
@@ -2,17 +2,20 @@
   :description "Crux in a Box"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [ch.qos.logback/logback-classic "1.2.3"]
-                 [ch.qos.logback/logback-core "1.2.3"]
-                 [org.slf4j/slf4j-api "1.7.30"]
 
                  ;; required:
                  [juxt/crux-core "21.02-1.15.0-beta"]
                  [juxt/crux-rocksdb "21.02-1.15.0-beta"]
 
+                 ;; logging:
+                 [org.clojure/tools.logging "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [ch.qos.logback/logback-core "1.2.3"]
+                 [org.slf4j/slf4j-api "1.7.30"]
+
                  ;; optional:
                  [juxt/crux-lucene "21.02-1.15.0-alpha"]
                  [juxt/crux-http-server "21.02-1.15.0-alpha"]]
 
+  ;; logging:
   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"])
