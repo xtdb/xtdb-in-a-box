21e5a4a3fa5c68cd06cba2ef939c5168ac08b53e
Steven Deobald <steven@deobald.ca>
2021-03-12 16:12:52 -0600

    Added Java `submitTx` example

--- java/src/main/java/cruxinabox/Box.java
+++ java/src/main/java/cruxinabox/Box.java
@@ -2,13 +2,29 @@ package cruxinabox;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+
 import crux.api.Crux;
 import crux.api.ICruxAPI;
+import crux.api.CruxDocument;
+import crux.api.TransactionInstant;
+import crux.api.tx.Transaction;
 
 class Box {
     public static void main(String[] args) {
-        try(ICruxAPI cruxNode = Crux.startNode(new File("config.json"))) {
-            System.console().writer().println("Started.");
+        try (ICruxAPI node = Crux.startNode(new File("config.json"))) {
+            System.out.println("Started.");
+
+            // submitTx example:
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("user/name", "zig");
+            CruxDocument document = CruxDocument.create("hi2u", data);
+            TransactionInstant transaction = node.submitTx(Transaction.buildTx(tx -> {
+                tx.put(document);
+            }));
+
+            // query example:
+
         }
         catch (IOException e) {
             // ...
