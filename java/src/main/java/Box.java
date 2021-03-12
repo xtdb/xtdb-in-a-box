82d2b5b7a8ce8376aec967d7688c670f2a662b68
Steven Deobald <steven@deobald.ca>
2021-03-12 14:30:05 -0600

    A small first pass at a Java version

---
+++ java/src/main/java/Box.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import crux.api.Crux;
+import crux.api.ICruxAPI;
+
+class Box {
+    public static void main(String[] args) {
+        try(ICruxAPI cruxNode = Crux.startNode(new File("config.json"))) {
+            System.console().writer().println("Started.");
+        }
+        catch (IOException e) {
+            // ...
+        }
+    }
+}
